d35a1e584a716bd899b43ff696196ac184beb026
bemeritus <bemerituss@gmail.com>
2024-12-09 21:05:55 +0500

    showing chirps done

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -4,11 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
+ 
+
 class Chirp extends Model
 {
     protected $fillable = [
         'message',
     ];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
     
 }
